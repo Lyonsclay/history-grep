9c89cd50f44a59b8623095c41063cc292021d6a2
Lyonsclay <lyonsclay@yahoo.com>
2023-07-16 00:11:43 +0300

    Use managed lists

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::{
     fs::File,
     io::{self, stdin, BufRead, BufReader, Error, ErrorKind},
     path::{Path, PathBuf},
-    string::String,
 };
 
 use clap::Parser;
@@ -13,9 +12,12 @@ use dialoguer::{console::Term, theme::ColorfulTheme, Select};
 
 #[derive(Parser)]
 struct Cli {
+    /// Sequence of search terms used to select matching lines
     pattern: Vec<String>,
+    /// Select a history file to search from home folder
     #[arg(long, default_value_t = false)]
     history: bool,
+    /// Select a complete file path to search from
     #[arg(short, long, default_value_t = false)]
     file: bool,
 }
@@ -24,12 +26,13 @@ struct History {
     home_path: PathBuf,
     shell_type: String,
     history_path: PathBuf,
+    history_list: Vec<String>,
     query_results: Vec<String>,
 }
 
 impl History {
     fn new() -> Self {
-        let shell = Self::get_str_from_env("SHELL")
+        let shell_type = Self::get_str_from_env("SHELL")
             .split("/")
             .last()
             .unwrap_or_else(|| "")
@@ -37,12 +40,13 @@ impl History {
 
         let home_path = Self::get_path_from_env("HOME");
         let mut history_path = home_path.clone();
-        history_path.push(&format!(".{}_history", shell));
+        history_path.push(&format!(".{}_history", shell_type));
 
         History {
             home_path,
-            shell_type: shell,
+            shell_type,
             history_path,
+            history_list: Vec::new(),
             query_results: Vec::new(),
         }
     }
@@ -60,7 +64,7 @@ impl History {
 }
 
 impl History {
-    fn get_hist_file_paths(self) -> Result<Vec<PathBuf>, VarError> {
+    fn get_hist_file_paths(&self) -> Result<Vec<PathBuf>, VarError> {
         let paths = fs::read_dir(&self.home_path).unwrap();
         let mut hist_paths: Vec<PathBuf> = Vec::new();
 
@@ -73,18 +77,48 @@ impl History {
 
         Ok(hist_paths)
     }
-}
 
-fn select_choose_file() -> bool {
-    println!("Would you like to choose a history file from your home folder?");
-    let items = vec!["yes", "no"];
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&items)
-        .default(0)
-        .interact()
-        .unwrap();
+    fn read_lines<P>(file_path: P) -> io::Result<io::Lines<BufReader<File>>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(file_path)?;
+        Ok(BufReader::new(file).lines())
+    }
 
-    items[selection] == "yes"
+    fn load_history(&mut self) {
+        let file_data = Self::read_lines(&self.history_path);
+        if let Ok(data) = file_data {
+            for line in data {
+                if let Ok(line_str) = line {
+                    self.history_list.push(line_str)
+                }
+            }
+        }
+    }
+
+    fn query_history(&mut self) {
+        let args = Cli::parse();
+        println!(
+            "Searching for - {:?} - in {:?}",
+            args.pattern, self.history_path
+        );
+            for line in &self.history_list {
+                    let mut found = false;
+                for arg in &args.pattern {
+                    if line.contains(&*arg) {
+                        found = true;
+                    } else {
+                        found = false;
+                        break;
+                    }
+                }
+                if found == true {
+                    self.query_results.push(line.to_owned());
+                    println!("{}", line.to_owned());
+                }
+            }
+    }
 }
 
 fn choose_file(items: Vec<PathBuf>) -> Result<PathBuf, Error> {
@@ -110,38 +144,6 @@ fn choose_file(items: Vec<PathBuf>) -> Result<PathBuf, Error> {
     }
 }
 
-fn read_lines<P>(file_path: P) -> io::Result<io::Lines<BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(file_path)?;
-    Ok(BufReader::new(file).lines())
-}
-
-fn query_file(file_path: PathBuf) {
-    let args = Cli::parse();
-    println!("Searching for - {:?} - in {:?}", args.pattern, file_path);
-    let file_data = read_lines(file_path);
-    if let Ok(data) = file_data {
-        for line in data {
-            if let Ok(line_str) = line {
-                let mut found = false;
-                for arg in &args.pattern {
-                    if line_str.contains(&*arg) {
-                        found = true;
-                    } else {
-                        found = false;
-                        break;
-                    }
-                }
-                if found == true {
-                    println!("{}", line_str);
-                }
-            }
-        }
-    }
-}
-
 fn get_file_path() -> PathBuf {
     println!("Please enter a valid file path:");
     let mut input = String::new();
@@ -153,7 +155,7 @@ fn get_file_path() -> PathBuf {
 
 fn main() {
     let args = Cli::parse();
-    let history = History::new();
+    let mut history = History::new();
     let search_path: PathBuf;
     if args.history {
         let paths = history
@@ -172,5 +174,7 @@ fn main() {
     } else {
         search_path = get_file_path()
     }
-    query_file(search_path)
+    history.history_path = search_path;
+    history.load_history();
+    history.query_history();
 }
